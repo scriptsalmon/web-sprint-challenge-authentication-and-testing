bae991da2b93f66dba96a1eb2bf28e3d3da46a53
scriptsalmon@gmail.com <scriptsalmon@gmail.com>
2021-11-12 14:03:22 -0800

    restricted middleware set

--- api/middleware/restricted.js
+++ api/middleware/restricted.js
@@ -1,5 +1,27 @@
+const jwt = require('jsonwebtoken');
+const { TOKEN_SECRET } = require('../../config');
+
 module.exports = (req, res, next) => {
-  next();
+
+  const token = req.headers.authorization
+
+  if(!token) {
+    return next({ status: 401, message: "token required" })
+  } 
+  jwt.verify(token, TOKEN_SECRET, (err, decoded) => {
+    if (err) {
+      return ({ 
+        status: 401,
+        message: `token invalid`
+      })
+    }
+
+  req.decodedJwt = decoded
+  console.log(decoded)
+  next()
+  })
+};
+
   /*
     IMPLEMENT
 
@@ -11,4 +33,3 @@ module.exports = (req, res, next) => {
     3- On invalid or expired token in the Authorization header,
       the response body should include a string exactly as follows: "token invalid".
   */
-};
