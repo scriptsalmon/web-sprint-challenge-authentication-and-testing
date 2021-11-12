748da7e8033224b68d5d8baae4858c1d87f3131c
scriptsalmon@gmail.com <scriptsalmon@gmail.com>
2021-11-12 14:28:36 -0800

    sanity check isnt passing now, for some reason... whats happening..

--- api/server.test.js
+++ api/server.test.js
@@ -1,4 +1,27 @@
-// Write your tests here
+const server = require('./server');
+const db = require('../data/dbConfig');
+const request = require('supertest');
+
+beforeAll( async () => {
+  await db.migrate.rollback()
+  await db.migrate.latest()
+});
+
+beforeEach( async () => {
+  await db.seed.run()
+});
+
+afterAll( async () => {
+  await db.destroy()
+});
+
 test('sanity', () => {
   expect(true).toBe(true)
+})
+
+describe('[POST] /auth/auth-router', () => {
+  test('/register responds with correct errors', async () => {
+    const res = await request(server).post('/api/auth')
+    expect(res.status).toBe(200);
+  })
 })
